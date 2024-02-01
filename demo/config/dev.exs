402ce02de4d818f22a4c81f85da52fca99db7df0
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-02-01 14:39:28 +0100

    Enable debug heex annotations

--- demo/config/dev.exs
+++ demo/config/dev.exs
@@ -43,3 +43,5 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 config :phoenix, :plug_init_mode, :runtime
+
+config :phoenix_live_view, debug_heex_annotations: true
