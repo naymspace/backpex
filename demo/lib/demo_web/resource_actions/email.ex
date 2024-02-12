3212f37332f491e2b0730b2aa851a9ac75922d1d
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-02-12 07:58:11 +0100

    Mark params as unused in email resource action

--- demo/lib/demo_web/resource_actions/email.ex
+++ demo/lib/demo_web/resource_actions/email.ex
@@ -37,7 +37,7 @@ defmodule DemoWeb.ResourceActions.Email do
   end
 
   @impl Backpex.ResourceAction
-  def handle(_socket, params) do
+  def handle(_socket, _params) do
     # Send mail
 
     # We suppose there was no error.
