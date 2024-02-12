6f9b0b4c0fa430d2a43954fcb8829414df5fe775
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-02-12 09:46:22 +0100

    Change order of callbacks

--- lib/backpex/item_actions/item_action.ex
+++ lib/backpex/item_actions/item_action.ex
@@ -188,7 +188,7 @@ defmodule Backpex.ItemAction do
   @callback handle(socket :: Phoenix.LiveView.Socket.t(), items :: list(map()), params :: map()) ::
               {:noreply, Phoenix.LiveView.Socket.t()} | {:reply, map(), Phoenix.LiveView.Socket.t()}
 
-  @optional_callbacks confirm: 1, confirm_label: 1, cancel_label: 1, fields: 0, changeset: 3
+  @optional_callbacks confirm: 1, confirm_label: 1, cancel_label: 1, changeset: 3, fields: 0
 
   @doc """
   Defines `Backpex.ItemAction` behaviour and provides default implementations.
