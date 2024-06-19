b24b02e90ecbf1feeb5c490af9fc55c1fee474a4
Simon Hansen <simon@sourceboat.com>
2024-06-19 09:29:15 +0200

    fix deprecation of live_flash

--- lib/backpex/html/layout.ex
+++ lib/backpex/html/layout.ex
@@ -127,19 +127,19 @@ defmodule Backpex.HTML.Layout do
   @doc type: :component
   attr(:flash, :map,
     required: true,
-    doc: "flash map that will be passed to `Phoenix.LiveView.Helpers.live_flash/2`"
+    doc: "flash map that will be passed to `Phoenix.Flash.get/2`"
   )
 
   def flash_messages(assigns) do
     ~H"""
     <div
-      :if={live_flash(@flash, :info) && live_flash(@flash, :info) != ""}
+      :if={Phoenix.Flash.get(@flash, :info) && Phoenix.Flash.get(@flash, :info) != ""}
       class="alert my-4 bg-blue-100 text-sm text-blue-800"
       phx-value-key="info"
     >
       <Heroicons.information_circle class="h-5 w-5" />
       <span>
-        <%= live_flash(@flash, :info) %>
+        <%= Phoenix.Flash.get(@flash, :info) %>
       </span>
       <div>
         <button
