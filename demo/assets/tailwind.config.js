ab6ebb0bd8a14404aa522a4b9f0cbf67ede0ad37
Thomas Fortes <thomas.fortes@gmail.com>
2024-07-05 09:12:00 -0300

    Reverting light theming

--- demo/assets/tailwind.config.js
+++ demo/assets/tailwind.config.js
@@ -4,7 +4,28 @@ const path = require('path')
 
 module.exports = {
   daisyui: {
-    themes: ["light", "dark", "cyberpunk", "retro"]
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes').light
+          ,
+          primary: '#1d4ed8',
+          'primary-content': 'white',
+          secondary: '#f39325',
+          info: '#dbeafe',
+          'info-content': '#2563eb',
+          'error-content': '#e11d48',
+          'base-200': '#f3f4f6',
+          'base-300': '#e5e7eb'
+        },
+        dark: {
+          ...require('daisyui/src/theming/themes').dark
+        },
+        cyberpunk: {
+          ...require('daisyui/src/theming/themes').cyberpunk
+        }
+      }
+    ]
   },
   content: [
     'assets/js/**/*.js',
