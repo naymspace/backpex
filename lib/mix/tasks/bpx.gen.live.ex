0a34b0d8a0162c9e351f97489ce86475d1101c6b
Mats Klein <mats.klein@naymspace.de>
2024-07-28 15:25:22 +0200

    add some docs to the bpx.gen.live

--- lib/mix/tasks/bpx.gen.live.ex
+++ lib/mix/tasks/bpx.gen.live.ex
@@ -1,5 +1,34 @@
 defmodule Mix.Tasks.Bpx.Gen.Live do
-  @shortdoc "Generates LiveView, templates, and context for a resource"
+  @shortdoc "Generates a Backpex.LiveResource for an existing ecto schema"
+
+  @moduledoc """
+  Generates a Backpex.LiveResource for an existing ecto schema.
+
+      mix bpx.gen.live Demo.User --file "user_live"
+
+  The first argument is the ecto schema module.
+  It takes the field names and types of this schema to generate a Backpex.LiveResource.
+  It has the minimum required code to display the `:index`, `:show`, `:new` and `:edit`.
+
+  The second argument defines the name for the LiveView file and its module name.
+  It will be copied to your projects `projectname_web/live` folder.
+
+
+  This generator will add the following file:
+
+    * a live_resource in `lib/app_web/live/user_live.ex`
+
+  After file generation is complete, there will be output regarding required
+  updates to the `lib/app_web/router.ex` file.
+
+      Add the live routes to your browser scope in lib/app_web/router.ex:
+
+      backpex_routes()
+
+      live_session :default, on_mount: [Backpex.InitAssigns] do
+        live_resources "/users", UserLive
+      end
+  """
 
   use Mix.Task
 
