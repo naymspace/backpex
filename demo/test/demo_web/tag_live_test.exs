52be83de167df71a2e80e3a38e27bea3650e2d31
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-08-26 09:56:24 +0200

    Add more tests for tag live resource

--- demo/test/demo_web/tag_live_test.exs
+++ demo/test/demo_web/tag_live_test.exs
@@ -23,11 +23,13 @@ defmodule DemoWeb.TagLiveTest do
 
       conn
       |> visit("/admin/tags")
+      |> assert_has(".table tbody tr", count: 2)
       |> unwrap(fn view ->
         view
         |> form("form[phx-change='index-search']", %{"index_search[value]" => "Elixir"})
         |> render_change()
       end)
+      |> assert_has(".table tbody tr", count: 1)
       |> refute_has("tr", text: "Phoenix")
       |> assert_has("tr", text: "Elixir")
     end
@@ -39,6 +41,67 @@ defmodule DemoWeb.TagLiveTest do
       test_delete_button_disabled_enabled(conn, "/admin/tags", tags)
       test_show_action_redirect(conn, "/admin/tags", tags)
       test_edit_action_redirect(conn, "/admin/tags", tags)
+    end
+  end
+
+  describe "tags live resource show" do
+    test "is rendered", %{conn: conn} do
+      tag = insert(:tag)
+
+      conn
+      |> visit("/admin/tags/#{tag.id}/show")
+      |> assert_has("h1", text: "Tag", exact: true)
+      |> assert_has("p", text: "Name", exact: true)
+      |> assert_has("p", text: "Inserted At", exact: true)
+      |> assert_has("p", text: tag.name, exact: true)
+    end
+  end
+
+  describe "tags live resource edit" do
+    test "is rendered", %{conn: conn} do
+      tag = insert(:tag)
+
+      conn
+      |> visit("/admin/tags/#{tag.id}/edit")
+      |> assert_has("h1", text: "Edit Tag", exact: true)
+      |> assert_has("button", text: "Cancel", exact: true)
+      |> assert_has("button", text: "Save", exact: true)
+    end
+
+    test "submit form", %{conn: conn} do
+      tag = insert(:tag, %{name: "Elixir"})
+
+      conn
+      |> visit("/admin/tags/#{tag.id}/edit")
+      |> unwrap(fn view ->
+        view
+        |> form("#resource-form", %{"change[name]" => "Phoenix"})
+        |> render_submit()
+      end)
+      |> assert_has(".table tbody tr", count: 1)
+      |> assert_has("p", text: "Phoenix", exact: true)
+    end
+  end
+
+  describe "tags live resource new" do
+    test "is rendered", %{conn: conn} do
+      conn
+      |> visit("/admin/tags/new")
+      |> assert_has("h1", text: "New Tag", exact: true)
+      |> assert_has("button", text: "Cancel", exact: true)
+      |> assert_has("button", text: "Save", exact: true)
+    end
+
+    test "submit form", %{conn: conn} do
+      conn
+      |> visit("/admin/tags/new")
+      |> unwrap(fn view ->
+        view
+        |> form("#resource-form", %{"change[name]" => "Phoenix"})
+        |> render_submit()
+      end)
+      |> assert_has(".table tbody tr", count: 1)
+      |> assert_has("p", text: "Phoenix", exact: true)
     end
   end
 end
