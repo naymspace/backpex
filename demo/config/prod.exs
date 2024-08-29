bf27b07fb2b2bc3727fe26b73c8c50858af106fc
Phil-Bastian Berndt <me@pbb.io>
2024-08-29 13:35:52 +0200

    remove demo SSL opts

--- demo/config/prod.exs
+++ demo/config/prod.exs
@@ -4,8 +4,4 @@ config :demo, DemoWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
-config :demo, Demo.Repo,
-  ssl: true,
-  ssl_opts: [verify: :verify_none]
-
 config :logger, backends: [:console, Sentry.LoggerBackend]
