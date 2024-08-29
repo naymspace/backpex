83e05cc1709ea601afd33c5909c64b298d31ca0e
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-08-29 13:41:56 +0200

    Remove landing page

---
+++ demo/lib/demo_web/controllers/redirect_controller.ex
@@ -0,0 +1,11 @@
+defmodule DemoWeb.RedirectController do
+  @moduledoc false
+
+  use DemoWeb, :controller
+
+  def redirect_to_users(conn, _params) do
+    conn
+    |> Phoenix.Controller.redirect(to: "/admin/users")
+    |> Plug.Conn.halt()
+  end
+end
