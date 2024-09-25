c779756bd96c0e536481d8496716cb5d2cc7cdd6
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2024-09-25 09:03:55 +0200

    Improve short link validation

--- demo/lib/demo/short_link.ex
+++ demo/lib/demo/short_link.ex
@@ -5,6 +5,9 @@ defmodule Demo.ShortLink do
   use Ecto.Schema
 
   import Ecto.Changeset
+  import Ecto.Query, warn: false
+
+  alias Demo.Repo
 
   @primary_key false
   schema "short_links" do
@@ -17,13 +20,17 @@ defmodule Demo.ShortLink do
   end
 
   @required_fields ~w[short_key url]a
+  @optional_fields ~w[product_id]a
 
   def changeset(short_link, attrs, _metadata \\ []) do
     short_link
-    |> cast(attrs, [:product_id | @required_fields])
+    |> cast(attrs, @required_fields ++ @optional_fields)
     |> cast_assoc(:product, with: &Demo.Product.changeset/2)
     |> add_short_key()
     |> validate_required(@required_fields)
+    |> validate_format(:short_key, ~r/^[A-Za-z0-9_-]+$/, message: "must be URL safe (only letters, numbers, underscores, and hyphens)")
+    |> validate_length(:short_key, min: 1, max: 64)
+    |> unique_constraint(:short_key)
   end
 
   defp add_short_key(changeset) do
@@ -33,11 +40,17 @@ defmodule Demo.ShortLink do
     end
   end
 
-  def generate_short_key do
-    :crypto.strong_rand_bytes(8)
-    |> Base.encode64()
-    |> String.replace("+", "-")
-    |> String.replace("/", "_")
-    |> String.replace("=", "")
+  def generate_unique_short_key do
+    key = generate_short_key()
+    if key_exists?(key), do: generate_unique_short_key(), else: key
   end
+
+  def generate_short_key(length \\ 8) do
+    length
+    |> :crypto.strong_rand_bytes()
+    |> Base.url_encode64(padding: false)
+    |> binary_part(0, length)
+  end
+
+  defp key_exists?(key), do: Repo.exists?(from s in __MODULE__, where: s.short_key == ^key)
 end
