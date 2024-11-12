f29f671bf9f28d8dc459e74b889492fe5c1b7eff
nedhgx <ned.hgx@gmail.com>
2024-11-12 11:32:45 -0800

    Add ‘rows' attribute to the textarea, with a default value of 3 rows.

--- lib/backpex/fields/textarea.ex
+++ lib/backpex/fields/textarea.ex
@@ -34,6 +34,7 @@ defmodule Backpex.Fields.Textarea do
         <BackpexForm.input
           type="textarea"
           field={@form[@name]}
+          rows={@field_options[:rows] || 3}
           translate_error_fun={Backpex.Field.translate_error_fun(@field_options, assigns)}
           phx-debounce={Backpex.Field.debounce(@field_options, assigns)}
           phx-throttle={Backpex.Field.throttle(@field_options, assigns)}
