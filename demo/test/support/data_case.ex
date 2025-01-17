e597e9aadfaea900ea1701f05f5b58f60937cf12
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-01-17 13:01:18 +0100

    Name variables consistently

--- demo/test/support/data_case.ex
+++ demo/test/support/data_case.ex
@@ -52,7 +52,7 @@ defmodule Demo.DataCase do
   """
   def errors_on(changeset) do
     Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
-      Regex.replace(~r"%{(\w+)}", message, fn _, key ->
+      Regex.replace(~r"%{(\w+)}", message, fn _match, key ->
         opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
       end)
     end)
