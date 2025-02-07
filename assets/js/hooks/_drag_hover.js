6a0b25b3d9617cb2479d982c2b5909b068afaabd
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-02-07 08:05:00 +0100

    Remove event listener on destroy

--- assets/js/hooks/_drag_hover.js
+++ assets/js/hooks/_drag_hover.js
@@ -4,12 +4,22 @@
 export default {
   mounted() {
     this.dragging = 0
-    this.el.addEventListener("dragenter", () => { this.dragChange(this.dragging + 1) })
-    this.el.addEventListener("dragleave", () => { this.dragChange(this.dragging - 1) })
-    this.el.addEventListener("drop", () => { this.dragChange(0) })
+
+    this.dragenterHandler = () => this.dragChange(this.dragging + 1)
+    this.dragleaveHandler = () => this.dragChange(this.dragging - 1)
+    this.dropHandler = () => this.dragChange(0)
+    
+    this.el.addEventListener("dragenter", this.dragenterHandler)
+    this.el.addEventListener("dragleave", this.dragleaveHandler)
+    this.el.addEventListener("drop", this.dropHandler)
   },
   dragChange(value) {
     this.dragging = value
     this.el.firstElementChild.classList.toggle("border-primary", this.dragging > 0)
+  },
+  destroyed() {
+    this.el.removeEventListener("dragenter", this.dragenterHandler)
+    this.el.removeEventListener("dragleave", this.dragleaveHandler)
+    this.el.removeEventListener("drop", this.dropHandler)
   }
 }
