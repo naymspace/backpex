c21e0d82829247ecd04b532f9523f1533ebf5306
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-02-07 07:48:48 +0100

    Update theme selector hook

--- assets/js/hooks/_theme_selector.js
+++ assets/js/hooks/_theme_selector.js
@@ -3,7 +3,6 @@
  */
 export default {
   mounted () {
-    const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
     const form = document.querySelector('#backpex-theme-selector-form')
     const storedTheme = window.localStorage.getItem('backpexTheme')
 
@@ -15,29 +14,33 @@ export default {
       activeThemeRadio.checked = true
     }
 
-    // Event listener that handles the theme changes and store
-    // the selected theme in the session and also in localStorage
-    window.addEventListener('backpex:theme-change', async (event) => {
-      const cookiePath = form.dataset.cookiePath
-      const selectedTheme = form.querySelector(
-        'input[name="theme-selector"]:checked'
+    window.addEventListener('backpex:theme-change', this.handleThemeChange.bind(this))
+  },
+  // Event listener that handles the theme changes and store
+  // the selected theme in the session and also in localStorage
+  async handleThemeChange() {
+    const form = document.querySelector('#backpex-theme-selector-form')
+    const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
+    const cookiePath = form.dataset.cookiePath
+    const selectedTheme = form.querySelector(
+      'input[name="theme-selector"]:checked'
+    )
+
+    if (selectedTheme) {
+      window.localStorage.setItem('backpexTheme', selectedTheme.value)
+      document.documentElement.setAttribute(
+        'data-theme',
+        selectedTheme.value
       )
-      if (selectedTheme) {
-        window.localStorage.setItem('backpexTheme', selectedTheme.value)
-        document.documentElement.setAttribute(
-          'data-theme',
-          selectedTheme.value
-        )
-        await fetch(cookiePath, {
-          body: `select_theme=${selectedTheme.value}`,
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/x-www-form-urlencoded',
-            'x-csrf-token': csrfToken
-          }
-        })
-      }
-    })
+      await fetch(cookiePath, {
+        body: `select_theme=${selectedTheme.value}`,
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded',
+          'x-csrf-token': csrfToken
+        }
+      })
+    }
   },
   // Call this from your app.js as soon as possible to minimize flashes with the old theme in some situations.
   setStoredTheme() {
@@ -46,5 +49,8 @@ export default {
     if (storedTheme != null) {
       document.documentElement.setAttribute('data-theme', storedTheme)
     }
+  },
+  destroyed() {
+    window.removeEventListener('backpex:theme-change', this.handleThemeChange.bind(this))
   }
 }
