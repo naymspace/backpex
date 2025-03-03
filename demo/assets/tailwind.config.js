118baaab0c5553c0402bb37a210681a293c41f89
Ido Leshkowitz <idoleshkowitz@gmail.com>
2025-03-03 13:52:26 +0200

    restored phx-* variants

--- demo/assets/tailwind.config.js
+++ demo/assets/tailwind.config.js
@@ -11,4 +11,9 @@ module.exports = {
       },
     },
   },
+  plugins: [
+    plugin(({ addVariant }) => addVariant('phx-click-loading', ['.phx-click-loading&', '.phx-click-loading &'])),
+    plugin(({ addVariant }) => addVariant('phx-submit-loading', ['.phx-submit-loading&', '.phx-submit-loading &'])),
+    plugin(({ addVariant }) => addVariant('phx-change-loading', ['.phx-change-loading&', '.phx-change-loading &'])),
+  ],
 };
