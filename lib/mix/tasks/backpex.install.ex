bbd67dfb8ef18d8949d39645ede3d8f02cf375d0
Mats Klein <mats.klein@naymspace.de>
2025-03-14 11:30:06 +0100

    generate igniter install task

---
+++ lib/mix/tasks/backpex.install.ex
@@ -0,0 +1,107 @@
+defmodule Mix.Tasks.Backpex.Install.Docs do
+  @moduledoc false
+
+  def short_doc do
+    "A short description of your task"
+  end
+
+  def example do
+    "mix backpex.install --example arg"
+  end
+
+  def long_doc do
+    """
+    #{short_doc()}
+
+    Longer explanation of your task
+
+    ## Example
+
+    ```bash
+    #{example()}
+    ```
+
+    ## Options
+
+    * `--example-option` or `-e` - Docs for your option
+    """
+  end
+end
+
+if Code.ensure_loaded?(Igniter) do
+  defmodule Mix.Tasks.Backpex.Install do
+    @shortdoc "#{__MODULE__.Docs.short_doc()}"
+
+    @moduledoc __MODULE__.Docs.long_doc()
+
+    use Igniter.Mix.Task
+
+    @impl Igniter.Mix.Task
+    def info(_argv, _composing_task) do
+      %Igniter.Mix.Task.Info{
+        # Groups allow for overlapping arguments for tasks by the same author
+        # See the generators guide for more.
+        group: :backpex,
+        # *other* dependencies to add
+        # i.e `{:foo, "~> 2.0"}`
+        adds_deps: [],
+        # *other* dependencies to add and call their associated installers, if they exist
+        # i.e `{:foo, "~> 2.0"}`
+        installs: [],
+        # An example invocation
+        example: __MODULE__.Docs.example(),
+        # A list of environments that this should be installed in.
+        only: nil,
+        # a list of positional arguments, i.e `[:file]`
+        positional: [],
+        # Other tasks your task composes using `Igniter.compose_task`, passing in the CLI argv
+        # This ensures your option schema includes options from nested tasks
+        composes: [],
+        # `OptionParser` schema
+        schema: [],
+        # Default values for the options in the `schema`
+        defaults: [],
+        # CLI aliases
+        aliases: [],
+        # A list of options in the schema that are required
+        required: []
+      }
+    end
+
+    @impl Igniter.Mix.Task
+    def igniter(igniter) do
+      # Do your work here and return an updated igniter
+      igniter
+      |> Igniter.add_warning("mix backpex.install is not yet implemented")
+
+      # TODO: Install Hooks in app.js
+      # TODO: Add Backpex to dependencies in mix.exs
+      # TODO: Add Backpex files to Tailwind content
+      # TODO: Update formatter configuration
+      # TODO: Create layout file
+      # TODO: Remove background color from body tag
+      # TODO: Configure daisyUI theme
+      # TODO: Add ThemeSelectorPlug to pipeline
+      # TODO: Remove @tailwindcss/forms plugin or switch to 'class' strategy
+      # TODO: Set up Heroicons and Tailwind plugin for icons
+    end
+  end
+else
+  defmodule Mix.Tasks.Backpex.Install do
+    @shortdoc "#{__MODULE__.Docs.short_doc()} | Install `igniter` to use"
+
+    @moduledoc __MODULE__.Docs.long_doc()
+
+    use Mix.Task
+
+    def run(_argv) do
+      Mix.shell().error("""
+      The task 'backpex.install' requires igniter. Please install igniter and try again.
+
+      For more information, see: https://hexdocs.pm/igniter/readme.html#installation
+      """)
+
+      exit({:shutdown, 1})
+    end
+  end
+end
