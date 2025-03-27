e80bc70d7eb0fec1df7f84c63627a37f261c56fb
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-03-27 09:59:12 +0100

    Update lib/backpex/gettext.ex
    Co-authored-by: Phil-Bastian Berndt <me@pbb.io>

--- lib/backpex/gettext.ex
+++ lib/backpex/gettext.ex
@@ -1,6 +1,4 @@
 defmodule Backpex.Gettext do
-  @moduledoc """
-  This module is mainly used to call gettext `*_noop` functions in order to automatically extract messages and create a `priv/gettext/backpex.pot` file.
-  """
+  @moduledoc false
   use Gettext.Backend, otp_app: :backpex
 end
