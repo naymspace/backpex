aa197c74ce9c7ef4678a00fe939819f8b898aed8
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-04-04 12:39:20 +0200

    Remove `handle_info/2` for embeds

--- lib/backpex/live_resource.ex
+++ lib/backpex/live_resource.ex
@@ -549,17 +549,6 @@ defmodule Backpex.LiveResource do
   end
 
   @impl Phoenix.LiveView
-  def handle_info({:put_embed, {key, value} = _assoc}, socket) do
-    changeset = Ecto.Changeset.put_embed(socket.assigns.changeset, key, value)
-    embeds = Map.get(socket.assigns, :embeds, []) |> Keyword.put(key, value)
-
-    socket
-    |> assign(:embeds, embeds)
-    |> assign(:changeset, changeset)
-    |> noreply()
-  end
-
-  @impl Phoenix.LiveView
   def handle_info({"backpex:" <> event, item}, socket) do
     handle_backpex_info({event, item}, socket)
   end
