f4a86829af153451c01ffb9f0bccac0618117fe3
Mats Klein <mats.klein@naymspace.de>
2025-04-10 05:22:21 +0200

    remove igniter_js from installs

--- lib/mix/tasks/backpex.install.ex
+++ lib/mix/tasks/backpex.install.ex
@@ -71,7 +71,6 @@ if Code.ensure_loaded?(Igniter) do
     @impl Igniter.Mix.Task
     def info(_argv, _composing_task) do
       %Igniter.Mix.Task.Info{
-        installs: [igniter_js: "~> 0.4.6"],
         adds_deps: [igniter_js: "~> 0.4.6"],
         example: __MODULE__.Docs.example(),
         schema: [app_js_path: :string, app_css_path: :string, no_layout: :boolean],
