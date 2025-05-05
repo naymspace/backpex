fcc5145707c7039a8e225dcc1d0fa13670a66f40
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-05-05 13:00:34 +0200

    Readd tests

--- demo/test/demo_web/browser/category_browser_test.exs
+++ demo/test/demo_web/browser/category_browser_test.exs
@@ -11,36 +11,35 @@ defmodule DemoWeb.Browser.CategoryBrowserTest do
     test "a11y", %{conn: conn} do
       conn
       |> visit(~p"/admin/categories")
-
-      # |> assert_a11y()
+      |> assert_a11y()
     end
   end
 
-  # describe "categories show" do
-  #   test "a11y", %{conn: conn} do
-  #     category = insert(:category)
+  describe "categories show" do
+    test "a11y", %{conn: conn} do
+      category = insert(:category)
 
-  #     conn
-  #     |> visit(~p"/admin/categories/#{category.id}/edit")
-  #     |> assert_a11y()
-  #   end
-  # end
+      conn
+      |> visit(~p"/admin/categories/#{category.id}/edit")
+      |> assert_a11y()
+    end
+  end
 
-  # describe "categories edit" do
-  #   test "a11y", %{conn: conn} do
-  #     category = insert(:category)
+  describe "categories edit" do
+    test "a11y", %{conn: conn} do
+      category = insert(:category)
 
-  #     conn
-  #     |> visit(~p"/admin/categories/#{category.id}/edit")
-  #     |> assert_a11y()
-  #   end
-  # end
+      conn
+      |> visit(~p"/admin/categories/#{category.id}/edit")
+      |> assert_a11y()
+    end
+  end
 
-  # describe "categories new" do
-  #   test "a11y", %{conn: conn} do
-  #     conn
-  #     |> visit(~p"/admin/categories/new")
-  #     |> assert_a11y()
-  #   end
-  # end
+  describe "categories new" do
+    test "a11y", %{conn: conn} do
+      conn
+      |> visit(~p"/admin/categories/new")
+      |> assert_a11y()
+    end
+  end
 end
