92d70ec5194671e5f65d9e7792cf9eca07cf9958
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-06-17 10:12:44 +0200

    Update `input/1` component

--- lib/backpex/fields/boolean.ex
+++ lib/backpex/fields/boolean.ex
@@ -68,7 +68,11 @@ defmodule Backpex.Fields.Boolean do
           id={"index-form-input-#{@name}-#{LiveResource.primary_value(@item, @live_resource)}"}
           type="toggle"
           field={@form[:value]}
-          input_class={["toggle toggle-sm toggle-success", "hover:input-bordered"]}
+          input_class={[
+            "toggle toggle-sm",
+            @valid && "toggle-primary",
+            !@valid && "toggle-error"
+          ]}
           phx-debounce={Backpex.Field.debounce(@field_options, assigns)}
           phx-throttle={Backpex.Field.throttle(@field_options, assigns)}
           readonly={@readonly}
