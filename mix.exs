9cefd8089ef7045ac436278fc9b63518a677a6a8
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-08-01 09:04:45 +0200

    Move changes to v0.15 upgrade guide

--- mix.exs
+++ mix.exs
@@ -181,6 +181,7 @@ defmodule Backpex.MixProject do
       "guides/translations/translations.md",
 
       # Upgrade Guides
+      "guides/upgrading/v0.15.md",
       "guides/upgrading/v0.14.md",
       "guides/upgrading/v0.13.md",
       "guides/upgrading/v0.12.md",
