cc2866e8cc4ace1767de7ca72be140c5a08f1cd5
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-08-01 10:13:26 +0200

    Run `mix format`

--- demo/config/config.exs
+++ demo/config/config.exs
@@ -48,7 +48,7 @@ config :esbuild,
     args:
       ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=. --alias:backpex=/opt/app),
     cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]},
+    env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]}
   ]
 
 config :tailwind,
