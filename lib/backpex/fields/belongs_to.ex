adc7c6d4f650fc27dd20c7d23d35d9ca8e30b79e
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-08-13 10:40:10 +0200

    Add `text-error-content` to index edit select inputs

--- lib/backpex/fields/belongs_to.ex
+++ lib/backpex/fields/belongs_to.ex
@@ -177,7 +177,11 @@ defmodule Backpex.Fields.BelongsTo do
           options={@options}
           prompt={@prompt}
           value={@value && Map.get(@value, :id)}
-          input_class={["select select-sm", @valid && "not-hover:select-ghost", !@valid && "select-error bg-error/10"]}
+          input_class={[
+            "select select-sm",
+            @valid && "not-hover:select-ghost",
+            !@valid && "select-error text-error-content bg-error/10"
+          ]}
           disabled={@readonly}
           hide_errors
         />
