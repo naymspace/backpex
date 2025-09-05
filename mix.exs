7ed6385a65167d5df592c2e7e454da4aada435b0
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-09-05 17:24:18 +0200

    Bump version in mix.exs

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Backpex.MixProject do
   use Mix.Project
 
-  @version "0.14.0"
+  @version "0.15.0"
 
   @source_url "https://github.com/naymspace/backpex"
   @changelog_url "https://github.com/naymspace/backpex/releases"
