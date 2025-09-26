c69b77c83188bf1714f9a57a8f90b87fb8606afa
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-09-26 09:37:22 +0200

    Add custom dropdown component

--- lib/backpex/html/core_components.ex
+++ lib/backpex/html/core_components.ex
@@ -20,4 +20,65 @@ defmodule Backpex.HTML.CoreComponents do
     <span class={[@name, @class]} {@rest} />
     """
   end
+
+  @doc """
+  Renders a dropdown menu component with a trigger and menu content.
+
+  ## Examples
+
+      <.dropdown id="user-menu">
+        <:trigger class="btn btn-primary btn-sm">
+          User Menu
+        </:trigger>
+        <:menu>
+          <li><.link navigate={~p"/profile"} class="menu-item">Profile</.link></li>
+          <li><.link navigate={~p"/settings"}>Settings</.link></li>
+          <li><.link navigate={~p"/logout"}>Logout</.link></li>
+        </:menu>
+      </.dropdown>
+  """
+  attr :id, :string, required: true, doc: "unique identifier for the dropdown"
+  attr :class, :any, default: nil, doc: "additional classes for the outer container element"
+
+  slot :trigger, doc: "the trigger element to be used to toggle the dropdown menu" do
+    attr :class, :string, doc: "additional classes for the wrapper of the trigger"
+  end
+
+  slot :menu, doc: "the dropdown menu" do
+    attr :class, :string, doc: "additional classes for the wrapper of the menu"
+  end
+
+  def dropdown(assigns) do
+    assigns =
+      assigns
+      |> update(:trigger, fn
+        [trigger] -> trigger
+        _trigger -> nil
+      end)
+      |> update(:menu, fn
+        [menu] -> menu
+        _trigger -> nil
+      end)
+
+    ~H"""
+    <div id={@id} class={["dropdown", @class]}>
+      <div id={"#{@id}-trigger"} role="button" tabindex="0" aria-haspopup="true" class={@trigger && @trigger[:class]}>
+        {render_slot(@trigger)}
+      </div>
+
+      <div
+        id={"#{@id}-menu"}
+        role="button"
+        tabindex="0"
+        aria-labelledby={"#{@id}-trigger"}
+        class={[
+          "menu dropdown-content z-[1] bg-base-100 rounded-box outline-black/5 shadow outline-[length:var(--border)]",
+          @menu && @menu[:class]
+        ]}
+      >
+        {render_slot(@menu)}
+      </div>
+    </div>
+    """
+  end
 end
