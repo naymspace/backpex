35bbcf339a7d4f0a77e256f0be7e62dae05c7944
thanos <thanosv@gmail.com>
2025-10-19 23:39:02 -0400

    adding :map to Backpex.Fields.InlineCRUD and a polymorphic example to demo

--- demo/lib/demo_web/live/person_live.ex
+++ demo/lib/demo_web/live/person_live.ex
@@ -1,5 +1,4 @@
-defmodule DemoWeb.PersonLive do
-alias Backpex.Fields.InlineCRUD
+defmodule DemoWeb.CarLive do
   use Backpex.LiveResource,
     adapter_config: [
       schema: Demo.Entity,
@@ -11,35 +10,43 @@ alias Backpex.Fields.InlineCRUD
     layout: {DemoWeb.Layouts, :admin},
     fluid?: true
 
-  @moduledoc """
-  This module just demos the use of a InlineCRUD set for type `:map`.
-  It also demos how you can use the power of `Backpex` to do a poor man's polymorphism by using `Backpex.LiveResource`
-  to control the values entered into the generic `:map` field while  reusing the same schema
-  for different entity types.
-  I
-  """
+
+    @moduledoc """
+    This module is similar to  `PersonLive` in that it demos the use of a InlineCRUD set for type `:map`.
+    It also demos how you can use the power of `Backpex` to do a poor man's polymorphism by using `Backpex.LiveResource`
+    to control the values entered into the generic `:map` field while  reusing the same schema
+    for different entity types.
+
+    It also is an example of how you could validate the values in the `:map` field by using a `validate`
+    function and calling `Backpex.Fields.InlineCRUD.changeset` inside the `changeset` function.
+    """
+
   import Ecto.Query, only: [where: 3]
 
   @impl Backpex.LiveResource
-  def singular_name, do: "Person"
+  def singular_name, do: "Car"
 
   @impl Backpex.LiveResource
-  def plural_name, do: "Persons"
+  def plural_name, do: "Cars"
+
 
   #
   # Added the item_query function to filter entities by type
   #
   def item_query(query, _, _assigns) do
     query
-    |> where([entity], entity.type=="person")
+    |> where([entity], entity.type=="car")
   end
 
+
   #
-  # Added the changeset function to create a changeset for a person entity
+  # Added the changeset function to create a new entity with type "car",
+  # and validate the child fields in the entity
   #
   def changeset(entity, params, metadata \\ []) do
     entity
-    |> Demo.Entity.changeset(params |> Map.put("type", "person"))
+    |> Demo.Entity.changeset(params |> Map.put("type", "car"))
+    |> Backpex.Fields.InlineCRUD.changeset(:fields, metadata)
   end
 
   @impl Backpex.LiveResource
@@ -47,7 +54,7 @@ alias Backpex.Fields.InlineCRUD
     [
       identity: %{
         module: Backpex.Fields.Text,
-        label: "Name",
+        label: "Model",
         searchable: true
       },
       fields: %{
@@ -56,23 +63,27 @@ alias Backpex.Fields.InlineCRUD
         type: :map,
         except: [:index],
         child_fields: [
-          email: %{
+          engine_size: %{
             module: Backpex.Fields.Text,
-            label: "Email"
+            label: "Engine Size (cc)",
+            input_type: :integer
           },
-          phone: %{
+          colour: %{
             module: Backpex.Fields.Text,
-            label: "Phone"
+            label: "Colour"
           },
-          age: %{
+          year: %{
             module: Backpex.Fields.Text,
-            label: "Age"
-          },
-          weight: %{
-            module: Backpex.Fields.Text,
-            label: "Weight (kg)"
+            label: "Year",
+            input_type: :integer
+
           }
-        ]
+        ],
+        validate: fn changeset->
+          changeset
+          |> Ecto.Changeset.validate_required([:colour, :year])
+          |> Ecto.Changeset.validate_number(:year, greater_than: 1900, less_than: Date.utc_today().year, message: "must be between 1900 and #{Date.utc_today().year}")
+        end
       },
     ]
   end
