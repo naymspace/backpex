29856abcdf96a3947375fc50f2cfa336dec9e604
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-10-24 15:42:52 +0200

    Add aria-label to select filter

--- lib/backpex/filters/select.ex
+++ lib/backpex/filters/select.ex
@@ -93,7 +93,7 @@ defmodule Backpex.Filters.Select do
 
   def render_form(assigns) do
     ~H"""
-    <select name={@form[@field].name} class="select select-sm mt-2">
+    <select name={@form[@field].name} class="select select-sm mt-2" aria-label={@prompt}>
       <option value="">{@prompt}</option>
       {Phoenix.HTML.Form.options_for_select(@options, selected(@value))}
     </select>
