ee8287feb0da61802371d780f9c9ad418134242f
thanos <thanosv@gmail.com>
2025-12-05 22:56:38 -0500

    removed input_type

--- demo/lib/demo_web/live/product_live.ex
+++ demo/lib/demo_web/live/product_live.ex
@@ -86,7 +86,6 @@ defmodule DemoWeb.ProductLive do
         align: :right
       },
       more_info: %{
-<<<<<<< HEAD
         module: Backpex.Fields.InlineCRUD,
         label: "More Info",
         type: :embed_one,
@@ -99,28 +98,9 @@ defmodule DemoWeb.ProductLive do
           goes_well_with: %{
             module: Backpex.Fields.Textarea,
             label: "Goes well with",
-            input_type: :textarea,
             rows: 5
           }
         ]
-=======
-              module: Backpex.Fields.InlineCRUD,
-              label: "More Info",
-              type: :embed_one,
-              except: [:index],
-              child_fields: [
-
-                weight: %{
-                  module: Backpex.Fields.Text,
-                  label: "Ave. Weight (kg)"
-                },
-                goes_well_with: %{
-                  module: Backpex.Fields.Textarea,
-                  label: "Goes well with",
-                  rows: 5
-                }
-              ],
->>>>>>> naymspace-develop
       },
       suppliers: %{
         module: Backpex.Fields.InlineCRUD,
